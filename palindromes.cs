03ef1a0293cb01a66f8ed7ec55b8d834b663247d
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-05 11:58:23 -0700

    add int functionality

--- palindromes.cs
+++ palindromes.cs
@@ -10,43 +10,44 @@ public class Program
     Console.WriteLine("Enter a word or number and I'll tell you if it's a palindrome!!");
     string userInput = Console.ReadLine();
 
-    // if (Regex.IsMatch(userInput, @"^\d+$"))
-    // {
-    //   int userInt = int.Parse(userInput);
-    //   bool isPalindrome = IntCheck(userInt);
-
-    //   if (isPalindrome)
-    //   {
-    //     Console.WriteLine("This is a palindrome!");
-    //   }
-    //   else
-    //   {
-    //     Console.WriteLine("Not a palindrome");
-    //   }
-
-    // }
-
-    bool isPalindrome = StringCheck(userInput);
-
-    if (isPalindrome)
+    if (Regex.IsMatch(userInput, @"^\d+$"))
     {
-      Console.WriteLine("This is a palindrome!");
+      int userInt = int.Parse(userInput);
+      bool isPalindrome = IntCheck(userInt);
+
+      if (isPalindrome)
+      {
+        Console.WriteLine("This is a palindrome!");
+      }
+      else
+      {
+        Console.WriteLine("Not a palindrome");
+      }
+
     }
+
     else
     {
-      Console.WriteLine("Not a palindrome!");
-    }
+      bool isPalindrome = StringCheck(userInput);
 
+      if (isPalindrome)
+      {
+        Console.WriteLine("This is a palindrome!");
+      }
+      else
+      {
+        Console.WriteLine("Not a palindrome!");
+      }
+
+    }
   }
+
+
 
   public static Boolean StringCheck(string inputString)
   {
     bool isPal;
-
     int len = inputString.Length;
-
-
-
     string newString = "";
 
     for (int i = 0; i < inputString.Length; i++)
@@ -64,16 +65,34 @@ public class Program
     {
       isPal = false;
     }
-
     return isPal;
-
-
-
   }
 
-  // public static Boolean IntCheck(int inputInt)
-  // {
-  //   bool ispal;
+  public static Boolean IntCheck(int inputInt)
+  {
+    bool ispal;
 
-  // }
+    int num = inputInt;
+    int temp = num;
+    int rem = 0;
+    int sum = 0;
+    while (num > 0)
+    {
+      rem = num % 10;
+      num = num / 10;
+      sum = sum * 10 + rem;
+    }
+
+    if (temp == sum)
+    {
+      ispal = true;
+    }
+    else
+    {
+      ispal = false;
+    }
+
+    return ispal;
+
+  }
 }
